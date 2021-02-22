153a70c0dedeed27315b98f3f93617a37240e02b
pivanta <bas677@icloud.com>
2021-02-22 23:46:05 +0000

    reduced refesh timer to 2 secs

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const startTimer = function(durationValue, clearInput, displayEl) {
                     userInputValue.value = '';
                     displayEl.textContent = "CountDown Completed!";
                     displayEl.style.color = 'yellow';
-                    setTimeout(refresh, 3000)
+                    setTimeout(refresh, 2000)
                 } else {
                     --durationValue;
         } 
